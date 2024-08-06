dda5ce34129f2c43522f92bdfc1635094e3258d5
monis_khan <monisykhan@gmail.com>
2024-08-07 00:21:26 +0530

    set up the user reducer and user api - done the user sign up process successfully

--- client/src/redux/api/userApi.ts
+++ client/src/redux/api/userApi.ts
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { User, UserResponseMessage } from "../../vite-env";
 
-const base = `${import.meta.env.VITE_SERVER}/api/v1/user/`
-
+const base = `${import.meta.env.VITE_SERVER}/api/v1/user`
+console.log('base:', base)
 export const user = createApi({
     reducerPath: "userApi",
     baseQuery: fetchBaseQuery({ baseUrl: base }),
